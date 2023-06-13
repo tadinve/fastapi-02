df1106c8b49c8a3c0dbdd062d36871b1188a8860
Venkatesh Tadinada <venkat@solivar.com>
2023-06-13 20:21:42 +0000

    setting baseline

--- app/main.py
+++ app/main.py
@@ -1,12 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from collections import defaultdict
-
-
-
-from sqlalchemy.inspection import inspect
+from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 
 from app.schema import Book as SchemaBook
@@ -23,29 +16,15 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-
 app = FastAPI()
-templates = Jinja2Templates(directory="app/templates")
-
-
-
-import pandas as pd
-def query_to_dict(rset):
-    result = defaultdict(list)
-    for obj in rset:
-        instance = inspect(obj)
-        for key, x in instance.attrs.items():
-            result[key].append(x.value)
-    return result
-
 
 # to avoid csrftokenError
-app.add_middleware(DBSessionMiddleware, db_url='postgresql://postgres:password123@postgres/fastapi')
+app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
 
 @app.get("/")
-async def root(request: Request,response_class=HTMLResponse):
-    return templates.TemplateResponse("base.html", 
-            {"request": request})    
+async def root():
+    return {"message": "hello world"}
+
 
 @app.post('/book/', response_model=SchemaBook)
 async def book(book: SchemaBook):
@@ -55,16 +34,11 @@ async def book(book: SchemaBook):
     return db_book
 
 @app.get('/books/')
-async def books(request: Request, response_class=HTMLResponse):
-    books = db.session.query(ModelBook).all()
-    df = pd.DataFrame(query_to_dict(books))
-    return templates.TemplateResponse("table.html", 
-            {"request": request, "table": df.to_html()})
+async def book():
+    book = db.session.query(ModelBook).all()
+    return book
 
-@app.get('/books/{book_id}')
-async def books(book_id: int):
-    # books = db.session.query(ModelBook).all()
-    return {"book": f"Description for book id {book_id}"}
+
   
 @app.post('/author/', response_model=SchemaAuthor)
 async def author(author:SchemaAuthor):
@@ -74,14 +48,10 @@ async def author(author:SchemaAuthor):
     return db_author
 
 @app.get('/authors/')
-async def authors():
-    authors = db.session.query(ModelAuthor).all()
-    return authors
-
-@app.get('/authors/{author_id}')
-async def authors(author_id: int):
-    author = db.session.query(ModelAuthor).get(author_id)
+async def author():
+    author = db.session.query(ModelAuthor).all()
     return author
+
 
 # To run locally
 if __name__ == '__main__':
